eaf02c4124ab306b3042df3eac980b264660ee52
“Shubham” <shubhampattar2@gmail.com>
2020-06-08 23:42:07 +0530

    UC4 Completed

--- gamblerSimulation.sh
+++ gamblerSimulation.sh
@@ -6,27 +6,38 @@ bet=1;
 wins=0;
 losses=0;
 count=0;
+dayCount=1;
 while [ 1 ]
 do
-	game=$((RANDOM%2));
-	if [ $game -eq 0 ]
+	if [ $dayCount -gt 20 ]; 
 	then
-		echo "Loss";
-		stake=$((stake-bet));
-		losses=$((losses+1));
-		if [ $losses -eq 50 ]
-		then
-			echo "Gambler Reached Maximum Losses";
-			break;
-		fi
+		echo "--- After 20 Days ---";
+		echo "Total Won = "$wins;
+		echo "Total Loss = "$losses;
+		echo "Balance = "$stake;
+		break;
 	else
-		echo "Won";
-		stake=$((stake+bet));
-		wins=$((wins+1));
-		if [ $wins -eq 50 ]
+		game=$((RANDOM%2));
+		if [ $game -eq 0 ]
 		then
-			echo "Gambler Reached Maximum Wins";
-			break;
+			echo "Loss";
+			stake=$((stake-bet));
+			losses=$((losses+1));
+			if [ $losses -eq 50 ]
+			then
+				echo "Gambler Reached Maximum Losses";
+				break;
+			fi
+		else
+			echo "Won";
+			stake=$((stake+bet));
+			wins=$((wins+1));
+			if [ $wins -eq 50 ]
+			then
+				echo "Gambler Reached Maximum Wins";
+				break;
+			fi
 		fi
 	fi
+	dayCount=$((dayCount+1));
 done
